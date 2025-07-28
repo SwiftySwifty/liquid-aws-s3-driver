5dbc110f57821373f33488e70594f092ecc72e29
Alex Sherbakov <mkll@users.noreply.github.com>
2025-07-28 22:23:49 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "LiquidAwsS3Driver", targets: ["LiquidAwsS3Driver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.2.0"),
+        .package(url: "https://github.com/SwiftySwifty/liquid-kit.git", from: "1.2.0"),
         .package(url: "https://github.com/soto-project/soto.git", from: "5.3.0")
     ],
     targets: [
